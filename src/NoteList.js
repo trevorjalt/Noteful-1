8411ff034d28d1d02e097142c141d2b160e08def
Trevor Alt <trevorjalt@gmail.com>
2020-09-02 10:35:08 -0400

    changes

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,14 +2,15 @@ import React from 'react';
 export default function NoteList(props) {
     console.log(props)
 
+
     let filteredNotes = props.notes.filter(note => {
         return note.folderId === props.match.params.folderId
     })  
     
     const mapper = (note) => (
         <div className="App-notes" key={note.id}>
-        <p>{note.name}</p>
-        <p>{note.modified}</p>
+        <h3>{note.name}</h3>
+        <p>Date modified on {note.modified}</p>
         <button name="delete" type="click">Delete</button>
     </div>
     )
